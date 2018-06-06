faebe9df9214bca38f090c9e17450e3f1077b14f
eric nicolas <eric.e.nicolas@gmail.com>
2018-06-06 16:18:55 -0700

    add TaskListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,7 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-
-
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course', 3),
-    new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
-
-  selectedTask: Task = null;
+  selectedTask=null;
 
   editTask(clickedTask) {
     this.selectedTask=clickedTask;
@@ -30,13 +22,4 @@ export class AppComponent {
     this.selectedTask = null;
   }
 
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
 }
